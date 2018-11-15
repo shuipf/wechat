b1ceb04c5cf2ec3b51461c0f1feacf5f41b4ac66
水平凡 <admin@abc3210.com>
2018-11-15 13:55:55 +0800

    优化用户相关

--- src/user/Remark.php
+++ src/user/Remark.php
@@ -1,32 +1,44 @@
 <?php
+// +----------------------------------------------------------------------
+// | Remark 设置用户备注名
+// | https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140838
+// +----------------------------------------------------------------------
+// | Copyright (c) 2019 http://www.shuipf.com, All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 水平凡 <admin@abc3210.com>
+// +----------------------------------------------------------------------
 
 namespace shuipf\wechat\user;
 
+use shuipf\wechat\bridge\AccessTokenTrait;
 use shuipf\wechat\bridge\Http;
 use shuipf\wechat\wechat\AccessToken;
 
 class Remark
 {
+    use AccessTokenTrait;
+
     /**
-     * 设置用户备注名.
+     * 设置用户备注名接口地址
      */
     const REMARK = 'https://api.weixin.qq.com/cgi-bin/user/info/updateremark';
 
     /**
-     * shuipf\wechat\Wechat\AccessToken.
-     */
-    protected $accessToken;
-
-    /**
-     * 构造方法.
+     * 构造方法
+     * Remark constructor.
+     * @param AccessToken $accessToken
      */
     public function __construct(AccessToken $accessToken)
     {
-        $this->accessToken = $accessToken;
+        $this->setAccessToken($accessToken);
     }
 
     /**
-     * 设置/更新用户备注.
+     * 设置/更新用户备注
+     * @param string $openid 用户标识
+     * @param string $remark 新的备注名，长度必须小于30字符
+     * @return \Doctrine\Common\Collections\ArrayCollection|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function update($openid, $remark)
     {
@@ -34,16 +46,14 @@ class Remark
             'openid' => $openid,
             'remark' => $remark,
         ];
-
         $response = Http::request('POST', static::REMARK)
             ->withAccessToken($this->accessToken)
             ->withBody($body)
             ->send();
-
+        //检查是否有错误
         if (0 != $response['errcode']) {
             throw new \Exception($response['errmsg'], $response['errcode']);
         }
-
         return $response;
     }
 }
--- src/user/User.php
+++ src/user/User.php
@@ -1,64 +1,75 @@
 <?php
+// +----------------------------------------------------------------------
+// | User 用户信息、列表相关
+// +----------------------------------------------------------------------
+// | Copyright (c) 2019 http://www.shuipf.com, All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 水平凡 <admin@abc3210.com>
+// +----------------------------------------------------------------------
 
 namespace shuipf\wechat\user;
 
+use shuipf\wechat\bridge\AccessTokenTrait;
 use shuipf\wechat\bridge\Http;
 use shuipf\wechat\wechat\AccessToken;
 use Doctrine\Common\Collections\ArrayCollection;
 
 class User
 {
+    use AccessTokenTrait;
+
     /**
-     * 获取用户信息.
+     * 获取用户信息
      */
     const USERINFO = 'https://api.weixin.qq.com/cgi-bin/user/info';
 
     /**
-     * 批量获取用户.
+     * 批量获取用户
      */
     const BETCH = 'https://api.weixin.qq.com/cgi-bin/user/info/batchget';
 
     /**
-     * 获取用户列表.
+     * 获取用户列表
      */
     const LISTS = 'https://api.weixin.qq.com/cgi-bin/user/get';
 
     /**
-     * shuipf\wechat\Wechat\AccessToken.
-     */
-    protected $accessToken;
-
-    /**
-     * 构造方法.
+     * 构造方法
+     * User constructor.
+     * @param AccessToken $accessToken
      */
     public function __construct(AccessToken $accessToken)
     {
-        $this->accessToken = $accessToken;
+        $this->setAccessToken($accessToken);
     }
 
     /**
-     * 查询用户列表.
+     * 查询用户列表
+     * @param null|string $nextOpenid 拉取列表的最后一个用户的OPENID
+     * @return ArrayCollection|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function lists($nextOpenid = null)
     {
         $query = null === $nextOpenid
             ? []
             : ['next_openid' => $nextOpenid];
-
         $response = Http::request('GET', static::LISTS)
             ->withAccessToken($this->accessToken)
             ->withQuery($query)
             ->send();
-
         if (0 != $response['errcode']) {
             throw new \Exception($response['errmsg'], $response['errcode']);
         }
-
         return $response;
     }
 
     /**
-     * 获取用户信息.
+     * 获取用户信息
+     * @param string $openid 普通用户的标识，对当前公众号唯一
+     * @param string $lang 返回国家地区语言版本，zh_CN 简体，zh_TW 繁体，en 英语
+     * @return ArrayCollection|string
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function get($openid, $lang = 'zh_CN')
     {
@@ -66,40 +77,37 @@ class User
             'openid' => $openid,
             'lang' => $lang,
         ];
-
         $response = Http::request('GET', static::USERINFO)
             ->withAccessToken($this->accessToken)
             ->withQuery($query)
             ->send();
-
         if (0 != $response['errcode']) {
             throw new \Exception($response['errmsg'], $response['errcode']);
         }
-
         return $response;
     }
 
     /**
-     * 批量获取用户信息.
+     * 批量获取用户信息
+     * @param array $openid 用户的标识，对当前公众号唯一
+     * @param string $lang 国家地区语言版本，zh_CN 简体，zh_TW 繁体，en 英语，默认为zh-CN
+     * @return ArrayCollection
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function getBetch(array $openid, $lang = 'zh_CN')
     {
         $body = [];
-
         foreach ($openid as $key => $value) {
             $body['user_list'][$key]['openid'] = $value;
             $body['user_list'][$key]['lang'] = $lang;
         }
-
         $response = Http::request('POST', static::BETCH)
             ->withAccessToken($this->accessToken)
             ->withBody($body)
             ->send();
-
         if (0 != $response['errcode']) {
             throw new \Exception($response['errmsg'], $response['errcode']);
         }
-
         return new ArrayCollection($response['user_info_list']);
     }
 }
